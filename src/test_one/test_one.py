ab0599e95a8d872d24a6c3c04463c37414f8a4ca
SMarkus27 <pereira.markus.27@hotmail.com>
2022-06-10 11:14:45 -0300

    exercises one and two

--- src/test_one/test_one.py
+++ src/test_one/test_one.py
@@ -6,13 +6,46 @@ If that's the case, your function should return those two numbers in array fashi
 
 
 def two_sum(array, target):
-        # Write your code here
-        pass
+    # Write your code here
+
+    """
+    This code was made in two parts.
+    The first part we used the quicksort algorithm to sort the array,avoiding the n*n complexity.
+    The second part we begin sum the extreme elements,and following to the middle. 
+    """
+    # Part one
+    def quicksort(array):
+        if len(array) <= 1:
+            return array
+        less_than_pivot = []
+        greater_than_pivot = []
+        pivot = array[0]
+        for value in array[1:]:
+            if value <= pivot:
+                less_than_pivot.append(value)
+            else:
+                greater_than_pivot.append(value)
+        return quicksort(less_than_pivot) + [pivot] + quicksort(greater_than_pivot)
+    
+    # Part two
+    new_array = quicksort(array)
+
+    initial = 0
+    final = len(new_array)-1
+    while initial < final:
+        result_sum = new_array[initial]+new_array[final]
+        if result_sum == target:
+            return [new_array[initial],new_array[final]]
+        elif result_sum < target:
+            initial +=1
+        else:
+            final-=1
+    return[]
 
 
 
 if __name__=="__main__":
-        array_one = [3, 5, -4, 8, 11, 1, -1, 6]
+        array_one = [3, 5, 4, 8, 11, 1, -1, 6]
         target = 10
 
         print(two_sum(array_one,target))
--- src/test_two/test_two.py
+++ src/test_two/test_two.py
@@ -5,13 +5,56 @@ Your function needs to determine whether the second array is contained in the fi
 
 def contains_second_array(array_one, array_two):
         # Write your code here
-        pass
 
+    """
+    This code was made in three parts.
+    The firs part we check if the size of array_one is smaller than array_two or the array_two is bigger than array_one,
+    if it is smaller or bigger return False.
+    The second part we used the quicksort algorithm to sort the arrays,avoiding the n*m complexity.
+    The third part we started to compare the initial elements, which would give us in the worst case an n+m complexity. 
+    """
+
+    # Part one
+    if len(array_one) < len(array_two) or len(array_two) > len(array_one):
+        return False
+    
+    # Part two     
+    def quicksort(array):
+        if len(array) <= 1:
+            return array
+        less_than_pivot = []
+        greater_than_pivot = []
+        pivot = array[0]
+        for value in array[1:]:
+            if value <= pivot:
+                less_than_pivot.append(value)
+            else:
+                greater_than_pivot.append(value)
+        return quicksort(less_than_pivot) + [pivot] + quicksort(greater_than_pivot)
+
+    new_array_one = quicksort(array_one)
+    new_array_two = quicksort(array_two)
+
+
+    # Part three
+    i , j = 0,0
+
+    while i < len(new_array_two) and j < len(new_array_one):
+        if new_array_one[j] < new_array_two[i]:
+            j += 1
+        elif new_array_one[j] == new_array_two[i]:
+            j += 1
+            i += 1
+        else:
+            return False
+
+
+    return False if i < len(array_two) else True
 
 
 if __name__=="__main__":
 
         array_one = [5, 1, 22, 25, 6, -1, 8, 10]
-        array_two = [1, 6, -1, 10]
+        array_two = [1, 6, 0, 10]
 
         print(contains_second_array(array_one, array_two))
